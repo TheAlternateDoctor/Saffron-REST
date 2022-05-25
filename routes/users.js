216ae247af93211a79f273d7dafba1d15d7048f6
TheAlternateDoctor <thealternatedoc@gmail.com>
2022-05-26 00:43:19 +0200

    cleaner users

--- routes/users.js
+++ routes/users.js
@@ -6,24 +6,37 @@ var router = express.Router();
 let db = new Mysql();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.post('/', function(req, res, next) {
-  res.send('respond with a specific code and make it')
-});
-
-router.get('/:uid', function(req, res, next) {
-  let id = req.params.uid;
-  let user = new User().find(db, id).then((result)=>{
+router.get('/', function (req, res, next) {
+  let users = User.findAll(db).then((result)=>{
     res.send(JSON.stringify(result));
+  }).catch((error)=>{
+
   });
 });
 
-router.get('/:uid/mods', function(req, res, next) {
+router.post('/', function (req, res, next) {
+  res.send('respond with a specific code and make it')
+});
+
+router.get('/:uid', function (req, res, next) {
   let id = req.params.uid;
-  res.send('get mods by user '+id);
+  let user = User.find(db, id).then((result) => {
+    res.send(JSON.stringify(result));
+  }).catch((error) => {
+    if (error === "result") {
+      res.statusCode = 404;
+      res.send('{"note":"No user with ID ' + id + '"}');
+    } else {
+      res.statusCode = 500;
+      res.send('{"note":"MySQL error.",' +
+        +JSON.stringify(error) + '}');
+    }
+  });
+});
+
+router.get('/:uid/mods', function (req, res, next) {
+  let id = req.params.uid;
+  res.send('get mods by user ' + id);
 });
 
 module.exports = router;
--- structures/user.js
+++ structures/user.js
@@ -6,16 +6,45 @@ User = class {
         this.githubHandle = github;
     }
 
-    find(db, id){
-        return new Promise((resolve)=>{
-            db.db.query('SELECT id, username, discord, youtube, github FROM User WHERE id="'+id+'"', function (err, result, fields) {
-                let user = {
-                    username : result[0].username,
-                    discordHandle : result[0].discord,
-                    youtubeHandle : result[0].youtube,
-                    githubHandle : result[0].github
+    static findAll(db){
+        return new Promise((resolve, reject)=>{
+            db.db.query('SELECT id, username, discord, youtube, github FROM User', function (err, result, fields) {
+                if(err){
+                    reject(err);
+                } else if(result.length === 0){
+                    reject("result");
+                } else{
+                    let users = new Array();
+                    result.forEach(element => {
+                        users.push({
+                            username : element.username,
+                            discordHandle : element.discord,
+                            youtubeHandle : element.youtube,
+                            githubHandle : element.github
+                        })
+                    });
+                    resolve(users);
                 }
-                resolve(user);
+            });
+        });
+    }
+
+    static find(db, id){
+        return new Promise((resolve, reject)=>{
+            db.db.query('SELECT id, username, discord, youtube, github FROM User WHERE id="'+id+'"', function (err, result, fields) {
+                if(err){
+                    reject(err);
+                } else if(result.length === 0){
+                    reject("result");
+                } else{
+                    let user = {
+                        username : result[0].username,
+                        discordHandle : result[0].discord,
+                        youtubeHandle : result[0].youtube,
+                        githubHandle : result[0].github
+                    }
+                    resolve(user);
+                }
             });
         });
     }
