f811ec3c67033e4614360f2b1d238443ce421b39
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-01-23 10:41:15 +0100

    fuck you *unjavascript your project* fuck you *java your project*
     Author:    TheAlternateDoctor <thealternatedoc@gmail.com>

---
+++ src/main/java/org/rhmodding/saffronrest/SaffronRestApplication.java
@@ -0,0 +1,13 @@
+package org.rhmodding.saffronrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaffronRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaffronRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rhmodding/saffronrest/SaffronRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhmodding.saffronrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaffronRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
