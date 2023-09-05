194ce24c65ef7c61939a2e5bca75f63daa74cb86
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-09-05 02:44:08 +0200

    Actually did the database connection

--- src/main/java/org/rhmodding/saffronrest/SaffronRestApplication.java
+++ src/main/java/org/rhmodding/saffronrest/SaffronRestApplication.java
@@ -1,9 +1,18 @@
 package org.rhmodding.saffronrest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= "org.rhmodding.saffronrest")
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+@EnableJpaRepositories("org.rhmodding.saffronrest.repositories")
 public class SaffronRestApplication {
 
 	public static void main(String[] args) {
