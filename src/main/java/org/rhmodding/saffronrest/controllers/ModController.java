64502eeb0d78f1be5adbf8487c5c7f006d7690ae
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-09-05 03:09:01 +0200

    Get one mod by ID

--- src/main/java/org/rhmodding/saffronrest/controllers/ModController.java
+++ src/main/java/org/rhmodding/saffronrest/controllers/ModController.java
@@ -6,6 +6,7 @@ import org.rhmodding.saffronrest.models.Mods;
 import org.rhmodding.saffronrest.repositories.ModsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class ModController {
     @GetMapping(value = "/mods")
     public List<Mods> getMods(){
         return modsRepository.findAll();
+    }
+
+    @GetMapping(value = "/mods/{id}")
+    public Mods getModById(@PathVariable Integer id){
+        return modsRepository.findById(id).get();
     }
 }
